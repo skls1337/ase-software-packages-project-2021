87aeaa7c0291f67f1da61e6f5388ab9268019089
Andy <andygrecu2000@gmail.com>
2021-05-01 11:52:23 +0300

    final commit

--- python/main.py
+++ python/main.py
@@ -2,6 +2,8 @@ import statsmodels.api as sm
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Declare a function to get the rolling statistics
+
 
 def get_trend(y, window):
     rollmean = y.rolling(window=window, center=False).mean()
@@ -9,20 +11,24 @@ def get_trend(y, window):
     return rollmean, rollstd
 
 
-# Read in a CSV file containing site usage data for company
+# Read from a CSV file data regarding tickets sales
+# over a period of time for several festivals
 df = pd.read_csv('./resources/festival_ticks_sales.csv')
 
 # Check data format
 print(df.head())
 
 
-# Check format again, then plot a line graphic
+# Plot a line graphic
 df.plot(kind='line', title='Number of tickets sold')
 plt.legend(bbox_to_anchor=(1.12, 1), loc='upper right', title='Festivals')
 plt.show()
 
 # Show trends (rolling avg, rolling std dev) over several windows: 1 week, 2 weeks, 1 month
 windows = [7, 14, 30]
+
+# Show trend evolution of Untold
+
 for window in windows:
     trendline = get_trend(df['Untold'], window=window)
     plt.plot(df['Untold'],  color='blue',
@@ -32,6 +38,9 @@ for window in windows:
              label=f'Trend {window} days (std. dev.)')
     plt.legend(loc='best')
     plt.show()
+
+# Show trend evolution of Neversea
+
 for window in windows:
     trendline = get_trend(df['Neversea'], window=window)
     plt.plot(df['Neversea'], color='blue',
@@ -41,6 +50,9 @@ for window in windows:
              label=f'Trend {window} days (std. dev.)')
     plt.legend(loc='best')
     plt.show()
+
+# Show trend evolution of Burning Man
+
 for window in windows:
     trendline = get_trend(df['Burning Man'], window=window)
     plt.plot(df['Burning Man'], color='blue',
